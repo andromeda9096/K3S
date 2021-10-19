20a8c66cf673be6777e24946ea99cec3c71cf4a1
trungnguyen <andromeda9096@gmail.com>
2021-10-19 10:15:44 +0700

    python

---
+++ docker-python/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html', title='Dockerize app', name='Python Container')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.environ['PORT'])
+
